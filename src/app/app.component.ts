eac8618d4c749a4ef4c6cc6c4ffc44a9eaf7b1ba
Shkyrmetoffa <shkyrmetoffa@gmail.com>
2016-09-12 16:18:49 +0300

    fizz-buzz was created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Observable} from  "rxjs/Observable";
+import "rxjs/add/observable/range";
+import "rxjs/add/operator/map"
 
 @Component({
   moduleId: module.id,
@@ -7,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public sequence: Observable<number|string>;
+
+  constructor(){
+    this.sequence = Observable.range(1, 100)
+      .map((item:number) => {
+        if(item % 3 === 0 || item % 5 === 0) {
+          if(item % 3 === 0 && item % 5 === 0) {
+            return "FizzBuzz";
+          }else if(item % 3 === 0) {
+            return "Fizz";
+          }else {
+            return "Buzz";
+          }
+        }else {
+          return item;
+        }
+      });
+
+    this.sequence.subscribe((item:number|string) => console.log(item));
+  }
 }
